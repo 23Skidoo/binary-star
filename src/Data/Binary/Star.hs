2cb481ac80b48a17d1ca24c59b173b833018aa90
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2009-04-02 08:34:46 +0200

    Some crazy experimentation.

--- src/Data/Binary/Star.hs
+++ src/Data/Binary/Star.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances #-}
 
 module Data.Binary.Star
     where
 
-lol :: String
-lol = "abcd"
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
+import Data.ByteString.Lazy (ByteString)
+
+data WrapPut t = WrapPut { unWrap ::t -> Put }
+
+type MyGet = Get
+type MyPut = WrapPut
+
+class Pickler d t where
+    pickle :: d t
+
+instance Binary a => Pickler Get a where
+    pickle = get
+
+instance Binary a => Pickler WrapPut a where
+    pickle = WrapPut { unWrap = put }
+
+myDecode :: Pickler Get a => ByteString -> a
+myDecode =  runGet pickle
+
+myEncode :: Pickler WrapPut a => a -> ByteString
+myEncode = runPut . unWrap pickle
+
+instance Serializable MyType where
+    serialize = do
